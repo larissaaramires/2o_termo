45bf8ea1d94e46fef3aa1c4b323c548593c97601
larissaaramires <larissa.souza.ramires@gmail.com>
2026-08-20 14:13:00 -0300

    20/08/2026

--- PBE/Exercicios/exercicio2.js
+++
@@ -1,9 +0,0 @@
-const entrada = require('readline-sync');
-function calcularMedia(n1, n2) {
-    return (n1 + n2) /2;
-}
-
-const numero1 = entrada.questionFloat("Digite a sua primeira nota: \n");
-const numero2 = entrada.questionFloat("Digite a sua segunda nota: \n");
-const resultado = calcularMedia(numero1, numero2);
-console.log(`A media calculada foi de: ${resultado}`);
